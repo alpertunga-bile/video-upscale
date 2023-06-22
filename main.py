c6a97d0af8b858cbd125d3c586320631eb31802f
alpertunga-bile <kingdom19online@hotmail.com>
2023-06-22 23:52:27 +0300

    Clearing is done in main and Completer class is added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from UpscaleUtility import Upscale
 
 from logging import Logger, getLogger, INFO, StreamHandler, Formatter
 from sys import stdout
+from os.path import exists
+from shutil import rmtree
 
 def SetLogger(logger : Logger):
     logger.setLevel(INFO)
@@ -10,10 +12,19 @@ def SetLogger(logger : Logger):
     handler.setLevel(INFO)
     formatter = Formatter('[%(levelname)s] %(message)s')
     handler.setFormatter(formatter)
-    logger.addHandler(handler) 
+    logger.addHandler(handler)
+
+def Clear():
+    if exists("temp"):
+        rmtree("temp")
+
+    if exists("tempOutput"):
+        rmtree("tempOutput")
 
 if __name__ == "__main__":
     logger = getLogger()
     SetLogger(logger)
 
     Upscale(logger)
+
+    Clear()
